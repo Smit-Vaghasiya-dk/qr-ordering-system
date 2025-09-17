b0493f9edffbcb03aec4abf7f164de33d6ba805e
smit-vaghasiya-dk <s225546319@deakin.edu.au>
2025-09-17 17:04:21 +1000

    Move forward towards the Cart

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # qr-ordering-system
+
+Introduce to the QR based ordering system.
+We are group of 3 members; 
+1. Om Sonani
+2. Smit Vaghasiya
+3. Naitik Rajpara
+
+We are performing this project under the boundry of provided requirements and use of technologies.
--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
-app.set('layout', 'layout'); // views/layout.ejs
-
+app.set("layout", "layout"); // views/layout.ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 // routes
-app.use('/api/menu', require('./routes/menu'));
-app.use('/api/orders', require('./routes/orders'));
+app.use("/api/menu", require("./routes/menu"));
+app.use("/api/orders", require("./routes/orders"));
 
 // pages
-app.get('/', (req, res) => res.redirect('/menu'));
-app.get('/menu', (req, res) => res.render('menu')); // menu page will fetch via API
-app.get('/dashboard', (req, res) => res.render('dashboard'));
+app.get("/", (req, res) => res.redirect("/menu"));
+app.get("/menu", (req, res) => res.render("menu")); // menu page will fetch via API
+app.get("/dashboard", (req, res) => res.render("dashboard"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
